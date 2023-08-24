2a18a2c26ef59fd73e5e2aee4690ac9eb8d66d8f
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-08-24 14:39:10 +0300

    Ready to deploy

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BarChart, SparkLineChart } from "@mui/x-charts";
 
 const Dashboard = () => {
-  return <div className="w-full h-screen Main--Page">Dashboard</div>;
+  return (
+    <div className="w-full h-screen Main--Page justify-center items-center">
+      <div className="w-full flex-[1/5] flex-row">
+        <div className="flex-[1/4]">
+          <SparkLineChart data={[1, 4, 2, 5, 7, 2, 4, 6]} />
+        </div>
+        <div className="flex-[1/4]"></div>
+        <div className="flex-[1/4]"></div>
+        <div className="flex-[1/4]"></div>
+      </div>
+      <div className="w-full flex=[3/5]"></div>
+      <div className="w-full flex-[1/5]"></div>
+    </div>
+  );
 };
 
 export default Dashboard;
