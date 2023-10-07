5423f07a888691dd313381bc8cadebd33d124668
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-10-07 19:20:29 +0300

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
         }
       });
     };
-    getUsersData();
+    if (token) getUsersData();
   }, [token]);
 
   useEffect(() => {
@@ -66,73 +66,76 @@ function App() {
 
   return (
     <div className={currentMode === "Dark" ? "dark" : ""}>
-      <Routes>
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-      <div className="flex w-screen h-screen relative dark:bg-main-dark-bg">
-        {activeMenu ? (
-          <div className="w-72 fixed sidebar dark:bg-logoColor bg-white animate-slide-in">
-            <Sidebar />
-          </div>
-        ) : (
-          <div className="w-0 dark:bg-logoColor animate-slide-out">
-            <Sidebar />
-          </div>
-        )}
+      {!token ? (
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      ) : (
+        <div className="flex w-screen h-screen relative dark:bg-main-dark-bg">
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-logoColor bg-white animate-slide-in">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-logoColor animate-slide-out">
+              <Sidebar />
+            </div>
+          )}
 
-        {/* Navbar + MainPage */}
-        <div
-          className={`dark:bg-background-logoColor bg-main-bg min-h-screen w-full ${
-            activeMenu ? "md:ml-72 Window--Page" : "flex-2"
-          }`}
-        >
-          {/* Navbar */}
-          <div className="fixed md:static flex dark:bg-background-logoColor bg-main-bg w-full items-center md:h-[8%] navbar">
-            <Navbar />
-          </div>
-
-          {/* Main page */}
+          {/* Navbar + MainPage */}
           <div
-            id="Main--Page"
-            className=" dark:bg-background-logoColor md:h-[92%] relative bg-white"
+            className={`dark:bg-background-logoColor bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72 Window--Page" : "flex-2"
+            }`}
           >
-            <Routes>
-              <Route path="/" element={<Dashboard socket={socket} />} />
-              <Route
-                path="/Dashboard"
-                element={<Dashboard socket={socket} />}
-              />
-              <Route path="/Orders" element={<Orders />} />
-              <Route path="/OilSamples" element={<OilSamples />} />
-              <Route
-                path="/OilSamplesAnalyzed"
-                element={<OilSamplesAnalyzed />}
-              />
-              <Route path="/Voice" element={<Voice />} />
-              <Route path="/Voice1" element={<Voice1 />} />
-              <Route path="/Video" element={<Video />} />
-              <Route
-                path="/Kanban"
-                element={<ManageKanban socket={socket} />}
-              />
-              <Route
-                path="/ManageKanban"
-                element={<Kanban socket={socket} />}
-              />
-              {links.map((item) => {
-                return (
-                  isUserAllowedCategory(item.name, usersData) && (
-                    <Route
-                      path={`/${item.name}/:tableName`}
-                      element={item.elem}
-                    />
-                  )
-                );
-              })}
-            </Routes>
+            {/* Navbar */}
+            <div className="fixed md:static flex dark:bg-background-logoColor bg-main-bg w-full items-center md:h-[8%] navbar">
+              <Navbar />
+            </div>
+
+            {/* Main page */}
+            <div
+              id="Main--Page"
+              className=" dark:bg-background-logoColor md:h-[92%] relative bg-white"
+            >
+              <Routes>
+                <Route path="/" element={<Dashboard socket={socket} />} />
+                <Route
+                  path="/Dashboard"
+                  element={<Dashboard socket={socket} />}
+                />
+                <Route path="/Orders" element={<Orders />} />
+                <Route path="/OilSamples" element={<OilSamples />} />
+                <Route
+                  path="/OilSamplesAnalyzed"
+                  element={<OilSamplesAnalyzed />}
+                />
+                <Route path="/Voice" element={<Voice />} />
+                <Route path="/Voice1" element={<Voice1 />} />
+                <Route path="/Video" element={<Video />} />
+                <Route
+                  path="/Kanban"
+                  element={<ManageKanban socket={socket} />}
+                />
+                <Route
+                  path="/ManageKanban"
+                  element={<Kanban socket={socket} />}
+                />
+                {links.map((item) => {
+                  return (
+                    isUserAllowedCategory(item.name, usersData) && (
+                      <Route
+                        path={`/${item.name}/:tableName`}
+                        element={item.elem}
+                      />
+                    )
+                  );
+                })}
+              </Routes>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
