4dd2b77522bd5d157198513fad323c6e30496459
mohamedSokker <mhmd.sokkar92@gmail.com>
2023-10-25 04:08:11 +0300

    Ready

--- src/data/allRoles.js
+++ src/data/allRoles.js
@@ -31,8 +31,8 @@ export const allSitesEqsdata = async (token) => {
     allEqs = [];
     allEqsWithName = [];
     for (let j = 0; j < dataEq.length; j++) {
-      allSites.push(dataEq[j].Location);
-      allSitesWithName.push({ name: dataEq[j].Location });
+      allEqs.push(dataEq[j].Location);
+      allEqsWithName.push({ name: dataEq[j].Location });
     }
     // dataEq.map((item) => {
     //   allEqs.push(item.Equipment);
