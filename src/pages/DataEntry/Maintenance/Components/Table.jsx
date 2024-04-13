d145d05bb97e7c8ecb0bf0eb825fbf423039fbc0
mohamedSokker <mhmd.sokkar92@gmail.com>
2024-04-13 18:00:07 +0200

    V3

--- src/pages/DataEntry/Maintenance/Components/Table.jsx
+++ src/pages/DataEntry/Maintenance/Components/Table.jsx
@@ -35,16 +35,6 @@ const Table = () => {
 
   let grid;
 
-  const formatDate = (anyDate) => {
-    dt = new Date(anyDate);
-    const year = dt.getFullYear();
-    let day = dt.getDate().toString();
-    let month = (Number(dt.getMonth()) + 1).toString();
-    if (month.length < 2) month = `0${month}`;
-    if (day.length < 2) day = `0${day}`;
-    return `${year}-${month}-${day}`;
-  };
-
   const getData = async () => {
     try {
       setLoading(true);
