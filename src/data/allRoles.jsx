37b4eaf9a7e21b6d320bd030d538d3f9dd47c3ff
mohamedSokker <mhmd.sokkar92@gmail.com>
2024-06-11 12:19:32 +0300

    Manage User update

--- src/data/allRoles.jsx
+++ src/data/allRoles.jsx
@@ -83,7 +83,7 @@ export const allData = async (token) => {
     ],
     StocksList: AllStocks,
     Tables: AllTables,
-    Catalogues: [],
+    Catalogues: ["true"],
     OilSamples: ["true"],
     OilSamplesAnalyzed: ["true"],
     ManageUsers: ["true"],
@@ -131,7 +131,7 @@ export const allDataWithName = async () => {
     ],
     StocksList: allStocksWithName,
     Tables: allTablesWithName,
-    Catalogues: [],
+    Catalogues: true,
     OilSamples: true,
     OilSamplesAnalyzed: true,
     ManageUsers: [
