ab711cf92f921776fbd4d0da162f955149a9512b
mohamedSokker <mhmd.sokkar92@gmail.com>
2025-05-03 06:23:52 +0300

    PowerBi Fix V3

--- src/components/Accessories/Dropdown.jsx
+++ src/components/Accessories/Dropdown.jsx
@@ -23,10 +23,10 @@ const FormalDropdown = ({
   const isValid = value !== "";
 
   return (
-    <div className="flex flex-col space-y-2">
+    <div className="flex flex-row items-center gap-2 text-[10px]">
       {/* Label */}
       {label && (
-        <label className="text-sm font-medium text-gray-700">
+        <label className="font-medium text-gray-700">
           {label}
           {required && <span className="text-red-500"> *</span>}
         </label>
@@ -37,25 +37,25 @@ const FormalDropdown = ({
         value={value}
         onChange={handleChange}
         onBlur={handleBlur}
-        className={`px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 ${
-          !isValid && isTouched
+        className={`px-2 py-1 border rounded-sm focus:outline-none focus:ring-1 ${
+          !isValid && isTouched && required
             ? "border-red-500 focus:ring-red-500"
-            : "border-gray-300 focus:ring-blue-500"
+            : "border-gray-300 focus:ring-[#CB1955]"
         }`}
       >
         <option value="" disabled>
           {placeholder}
         </option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
+        {options.map((option, i) => (
+          <option key={i} value={option}>
+            {option}
           </option>
         ))}
       </select>
 
       {/* Error Message */}
-      {!isValid && isTouched && (
-        <p className="text-sm text-red-500">{errorMessage}</p>
+      {!isValid && isTouched && required && (
+        <p className="text-[10px] text-red-500">{errorMessage}</p>
       )}
     </div>
   );
